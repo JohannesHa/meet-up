de3bc6cbafe656d3bf9f5a1881df80487bc1fa19
Johannes <johanneshage97@gmail.com>
2018-09-08 00:24:40 +0200

    initialised project and ran truffle unbox drizzle

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router'
+import HomeContainer from './layouts/home/HomeContainer'
+import axios from 'axios';
+import config from '../config';
+
+// Styles
+import './css/oswald.css'
+import './css/open-sans.css'
+import './css/pure-min.css'
+import './App.css'
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
+
+    var pinata_api_key = config.MY_KEY;
+    var pinata_secret_api_key = config.SECRET_KEY;
+
+    this.state = {
+      testingAxios: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios
+      .get("https://api.pinata.cloud/data/testAuthentication", {
+        headers: {
+          'pinata_api_key': pinata_api_key,
+          'pinata_secret_api_key': pinata_secret_api_key
+        }
+      })
+      .then(function (response) {
+        //handle your response here
+        const testingAxios = response.data;
+        this.setState({ testingAxios });
+      })
+      .catch(function (error) {
+        //handle error here
+        console.log("Error: " + error)
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={HomeContainer} />
+        <ul>
+          {console.log("tested Axios:" + this.state.testingAxios)}
+          {this.state.testingAxios.map(testingAxios => <li>{testingAxios.message}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App
