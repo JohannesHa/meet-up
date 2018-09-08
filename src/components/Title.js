863cc05c15ec13490cb7552a2a358db4eca0584d
Jack Leslie <jjdl@st-andrews.ac.uk>
2018-09-08 18:35:17 +0200

    Re factored buttons and text into components, made responsive

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { colors, fontSizes, width } from '../styles.js';
 
 const StyledTitle = styled.h1`
   text-decoration: none;
-  color: inherit;
+  color: ${colors.title};
+  font-size: ${fontSizes.title};
   margin: 0;
   padding: 0;
+
+  @media only screen and (max-width: ${width.mobile}) {
+    font-size: ${fontSizes.titleMobile};
+  }
 `;
 
 const Title = ({ children, ...props }) => (
--- src/styles.js
+++ src/styles.js
@@ -6,7 +6,23 @@ export const colors = {
   dark: `#171439`,
   darker: `#0B0033`,
   purple: '#7b40ff',
+  purpler: '#6C34EB',
+  purplest: '#6331D4',
   metamask: '#F6851B',
   status: '#4957B8',
+  title: '#FFFFFF',
   subtitle: '#CCCCCC',
 };
+
+export const fontSizes = {
+  title: '30px',
+  titleMobile: '20px',
+  subtitle: '20px',
+  subtitleMobile: '14px',
+  text: '14px',
+  textMobile: '11px',
+};
+
+export const width = {
+  mobile: '600px',
+}
