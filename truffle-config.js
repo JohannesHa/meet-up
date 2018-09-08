75c78b5d84d8b8820a9beb49c55af0214c8b448a
sabinebertram <sabine.bertram@mailbox.org>
2018-09-09 01:28:50 +0200

    adds rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = '';
+const token = '';
+
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -5,6 +11,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + token)
+      },
+      network_id: '4'
     }
   }
 };
