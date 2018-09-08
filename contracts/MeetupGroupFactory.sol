9164b2f02352d75acb150fcfc4a0326c9273f5da
sabinebertram <sabine.bertram@mailbox.org>
2018-09-08 04:10:19 +0200

    meetup group factory, not done

---
+++ contracts/MeetupGroupFactory.sol
@@ -0,0 +1,43 @@
+pragma solidity 0.4.24;
+
+import "./zeppelin/ownership/Ownable.sol";
+import "./MeetupGroup.sol";
+
+contract MeetupGroupFactory is Ownable {
+
+    address[] public groups;
+
+    event CreateGroup(address groupId, string name, string description, string ens, address userId);
+
+    /* Public functions */
+    /**
+     * @dev Create Group.
+     * @param _name The name of the meetup group
+     * @param _geohash Geohash of the meetup group
+     * @param _category Category the meetup group belongs to
+     * @param _description Desciption of the meetup group
+     * @param _logo IPFS link
+     */
+    function createGroup(
+        string _name,
+        bytes8 _geohash,
+        string _category,
+        string _description,
+        bytes _logo
+    ) public {
+
+        // create ens here
+
+        address group = new MeetupGroup(
+        msg.sender,
+        _name,
+        _ens,
+        _geohash,
+        _category,
+        _description,
+        _logo
+        );
+
+        emit CreateGroup(group, _name, _description, _ens, msg.sender);
+    }
+}
