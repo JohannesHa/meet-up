20f66e2b7dbcc34904e8fddb5363e8104a43a17d
Dominik Scherm <dominik@dnddev.com>
2018-09-09 10:52:15 +0200

    Added graphQL for GroupDetail

--- src/layouts/groupDetail/GroupDetailContainer.js
+++ src/layouts/groupDetail/GroupDetailContainer.js
@@ -5,8 +5,10 @@ import React from 'react';
 import Group from '../../components/Group/Group';
 import EventList from '../../components/EventList';
 import Subtitle from '../../components/Subtitle';
+import {withApollo, compose, graphql} from 'react-apollo';
+import gql from 'graphql-tag';
 
-const GroupDetailContainer = ({ location, onSelectEvent }) => {
+const GroupDetailContainer = ({ location, onSelectEvent, error, loading, groupByEns }) => {
   return (
     <div>
       <Group address={location.pathname} name="Blockchain Berlin" memberCount="100" country="Germany" region="Berlin" category="Blockchain" isAdmin={false} isMember={true}/>
@@ -17,4 +19,26 @@ const GroupDetailContainer = ({ location, onSelectEvent }) => {
   )
 };
 
-export default GroupDetailContainer;
+const GroupByEnsQuery = gql`
+  query groupsByEns($ens: String!)  {
+      groups(where: {ens: $ens}) {
+          name
+          memberCount
+          country
+          region
+          category
+          events {
+              name
+              attendees
+              maxAttendees
+          }
+      }
+  }
+`;
+
+export default compose(
+  withApollo,
+  graphql(GroupByEnsQuery, {
+
+  })
+)(GroupDetailContainer)
